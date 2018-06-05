c641658305f38432834a83a37dc9cba90c60968a
matheusnetzee <matheus.borro@netzee.com.br>
2018-06-05 13:32:12 -0300

    adicionando favicon

--- resources/views/layouts/structure.blade.php
+++ resources/views/layouts/structure.blade.php
@@ -4,7 +4,8 @@
 	    <meta charset="utf-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
-    	<title>{{ config('app.name', 'Easy Tools') }}</title>
+    	{{-- <title>{{ config('app.name', 'Easy Tools') }}</title> --}}
+    	<title>Easy Tools</title>
 
     	<!-- CSRF Token -->
     	<meta name="csrf-token" content="{{ csrf_token() }}">
@@ -15,6 +16,7 @@
     	<link href="{{ asset('css/slick.min.css') }}" rel="stylesheet" >
     	<link href="{{ asset('css/index.css') }}"  rel="stylesheet" >
 		<link href="{{ asset('css/fontawesome.min.css') }}" rel="stylesheet">
+		<link rel="icon shortcut" type="image/gif" href="{{ asset('img/favicon.svg') }}">
 
 		<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->		
 		<!-- <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700,900" rel="stylesheet"> -->
