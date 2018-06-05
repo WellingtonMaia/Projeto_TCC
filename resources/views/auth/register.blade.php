1357a743b0f2a66393c778e7d7d03e4294520fee
matheusnetzee <matheus.borro@netzee.com.br>
2018-06-05 10:17:17 -0300

    arrumando template de registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    <div class="limiter">
+        <div class="container-login100">
+            <div class="wrap-login100">
+                <form class="login100-form validate-form"  method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+
+                    <span class="login100-form-title p-b-48">
+                        <img src="img/logo.png">
+                    </span>
+
+                    <div class="wrap-input100 validate-input {{ $errors->has('name') ? ' has-error' : '' }}" data-validate = "Enter name">
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                        <input class="input100" type="text" name="email">
+                        <span class="focus-input100" data-placeholder="Name"></span>
+                    </div>
+
+
+                    <div class="wrap-input100 validate-input {{ $errors->has('email') ? ' has-error' : '' }}" data-validate = "Valid email is: a@b.c">
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                        <input class="input100" type="text" name="email">
+                        <span class="focus-input100" data-placeholder="Email"></span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input{{ $errors->has('password') ? ' has-error' : '' }}" data-validate="Enter password">
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                        <span class="btn-show-pass">
+                            <i class="zmdi zmdi-eye"></i>
+                        </span>
+                        <input class="input100" type="password" name="password">
+                        <span class="focus-input100" data-placeholder="Password"></span>
+                    </div>                    
+
+                    <div class="wrap-input100 validate-input">
+                        <span class="btn-show-pass">
+                            <i class="zmdi zmdi-eye"></i>
+                        </span>
+                        <input class="input100" type="password" name="password_confirmation">
+                        <span class="focus-input100" data-placeholder="Confirm Password"></span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input {{ $errors->has('role') ? ' has-error' : '' }}" data-validate = "Role is required">
+                        @if ($errors->has('role'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('role') }}</strong>
+                            </span>
+                        @endif
+                        <input class="input100" type="text" name="role">
+                        <span class="focus-input100" data-placeholder="Role"></span>
+                    </div>
+
+                    <div class="container-login100-form-btn">
+                        <div class="wrap-login100-form-btn">
+                            <div class="login100-form-bgbtn"></div>
+                            <button class="login100-form-btn">
+                                Register
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+{{--<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -86,5 +166,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
