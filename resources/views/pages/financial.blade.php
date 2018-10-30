cce0653c3b7eb0f81955219da274b9bc2be32cd9
MatheusShirakawa <matheus_shirakawa@outlook.com>
2018-10-30 20:15:53 -0300

    adicionando novos controllers e arrumando blade do financeiro

--- resources/views/pages/financial.blade.php
+++ resources/views/pages/financial.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.ample')
 @section('content')
-
-	 <div class="financial-content">
-    <div class="container-fluid">
-      <div class="col-md-12">
-          <div class="card">
-
-              <div class="block-icon">
-                <svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="{{ asset('img/sprite.svg#financeiro') }}"></use></svg>
-              </div>
-              <div class="list-content">
-                  <h4 class="title-add">Financeiro</h4>
-                  @if (session('status'))
-                      <div class="alert alert-success">
-                          {{ session('status') }}
-                      </div>
-                  @endif
+<div id="page-wrapper">
+  <div class="container-fluid">
+      <div class="row bg-title">
+          <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+              <h4 class="page-title">Profile page</h4> </div>
+          <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+              {{-- <a href="https://wrappixel.com/templates/ampleadmin/" target="_blank" class="btn btn-danger pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Upgrade to Pro</a> --}}
+              <ol class="breadcrumb">
+                  <li><a href="{{ route('home') }}">Dashboard</a></li>
+                  <li class="active"> Financeiro</li>
+              </ol>
+          </div>
+      </div>
+    <div class="col-md-12">
+        <div class="white-box">
+          <div class="list-content">
                   <div class="table-responsive">
                     <table class="table">
                         <thead>
